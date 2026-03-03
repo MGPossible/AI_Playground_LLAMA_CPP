da8018419f15b2c5dfcc59234adb8a2c653c7de8
MGPossible <sinners.ger@icloud.com>
2026-03-03 16:08:17 +0100

    Remove unnecessary lines and simplify Start.bat

--- Start.bat
+++ Start.bat
@@ -1,5 +1,6 @@
 @echo off
     
-start .\llamacpp\llama-server.exe  --models-dir ./models --host 127.0.0.1 --port 8033 -ngl 99
+.\llamacpp\llama-server.exe  --models-dir ./models --host 127.0.0.1 --port 8033 -ngl 99
 
 pause
+
